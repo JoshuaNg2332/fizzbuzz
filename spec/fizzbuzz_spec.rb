20d7a5d7c439f433570f08679b1cc808e6036ba7
Josh <joshua.ng2332@gmail.com>
2020-06-01 11:28:51 +0100

    finish exercise

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,27 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(15)).to eq '1'
+  end
+  it 'returns "2" when passed 3' do
+    expect(fizzbuzz(15)).to eq '3'
+  end
+  it 'returns "4" when passed 4' do
+    expect(fizzbuzz(15)).to eq '4'
+  end
+  it 'returns "7" when passed 7' do
+    expect(fizzbuzz(15)).to eq '7'
+  end
+  it 'returns "8" when passed 8' do
+    expect(fizzbuzz(15)).to eq '8'
+  end
+
+
 end
